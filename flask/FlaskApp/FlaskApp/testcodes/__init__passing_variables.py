9780564758276562bdceefdec3c23bc162b99908
mayank <m.charly3015@gmail.com>
2017-03-03 04:08:16 +0530

    controller+logic

---
+++ flask/FlaskApp/FlaskApp/testcodes/__init__passing_variables.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+	
+	return render_template("index.html", title="my first page", paragraph=["it's going fine"," i am learnig great"])
+
+
+
+if __name__ == "__main__":
+	app.run()
