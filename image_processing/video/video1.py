7b003be5227c7038f83bbd54de04c5942fc33475
mayank <m.charly3015@gmail.com>
2017-03-19 06:34:32 +0530

    video streaming code

--- image_processing/video/video1.py
+++ image_processing/video/video1.py
@@ -3,21 +3,24 @@ import cv2
 
 
 capture = cv2.VideoCapture(0)
+winName = "Test Capture"
+cv2.namedWindow(winName, cv2.CV_WINDOW_AUTOSIZE)
 
 while(True):
 
 	# Capture frame-by-frame
 
-	ret, frame = capture.read()
+	#defining operation on  for converting to grayscale
 
-	#defining operation on frame
-
-	grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+	#gray = cv2.cvtColor(capture.read()[1], cv2.COLOR_RGB2GRAY)
+	
+	# for capturing coloured video
+	
+	ret, gray = capture.read()
 
 	#Display the resulting frame
 
-	cv2.imshow('frame', grayscale)
+	cv2.imshow(winName, gray)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
