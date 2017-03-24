e59a2f81051dff674f1959754f6a5a2e4ddb0c83
mayank <m.charly3015@gmail.com>
2017-03-24 19:32:23 +0530

    first ui working

--- pest_detection/pestsite/pest_classification/views.py
+++ pest_detection/pestsite/pest_classification/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from django.http import HttpResponse
+from django.template import loader
 
 import requests
 import json
@@ -14,19 +15,22 @@ def test(request):
 	return HttpResponse("Second Page")
 
 def profile(request):
-    jsonList = []
-    req = requests.get('https://api.github.com/users/Mayank-Chaudhari9')
-    jsonList.append(json.loads(req.content))
     parsedData = []
-    userData = {}
-    for data in jsonList:
-    	userData['name'] = data['name']
-        userData['blog'] = data['blog']
-        userData['email'] = data['email']
-        userData['public_gists'] = data['public_gists']
-        userData['public_repos'] = data['public_repos']
-        userData['avatar_url'] = data['avatar_url']
-        userData['followers'] = data['followers']
-        userData['following'] = data['following']
-    parsedData.append(userData)
-    return HttpResponse(parsedData)
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        req = requests.get('https://api.github.com/users/' + username)
+        jsonList = []
+        jsonList.append(json.loads(req.content))
+        userData = {}
+        for data in jsonList:
+            userData['name'] = data['name']
+            userData['blog'] = data['blog']
+            userData['email'] = data['email']
+            userData['public_gists'] = data['public_gists']
+            userData['public_repos'] = data['public_repos']
+            userData['avatar_url'] = data['avatar_url']
+            userData['followers'] = data['followers']
+            userData['following'] = data['following']
+        parsedData.append(userData)
+    #return HttpResponse(parsedData)
+    return render(request, 'pest_classification/profile.html',{'data': parsedData})
