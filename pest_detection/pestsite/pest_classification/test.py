649a5bfeaf04e6561ffb03b5f716c4573c639506
mayank <m.charly3015@gmail.com>
2017-04-14 16:23:40 +0530

    integration test  2 complete

--- pest_detection/pestsite/pest_classification/test.py
+++ pest_detection/pestsite/pest_classification/test.py
@@ -1,13 +1,18 @@
 import sys
 
 #sys.path.insert(0,'/home/devil/Desktop/UntitledFolder/running_projects/pest_detection/pestsite')
-sys.path.insert(0,'/home/devil/Desktop/UntitledFolder')
+sys.path.insert(0,'/home/devil/tensorflow/tf_files')
 
-import test2
+import label_fun
 
 def hello():
 	print "all this work is waste oooh lalalallaaaaa"
 	y=" script integration successful"
-	return y + " " + test2.fun()
+	labels = "/home/devil/tensorflow/tf_files/retrained_labels.txt"
+	graph = "/home/devil/tensorflow/tf_files/retrained_graph.pb"
+	image = "/home/devil/tensorflow/tf_files/testing_images/betal/test1.jpg"
+
+
+	return y + " " + label_fun.label(labels, image, graph)
 
 hello()
--- pest_detection/pestsite/pest_classification/views.py
+++ pest_detection/pestsite/pest_classification/views.py
@@ -72,27 +72,42 @@ def login_process(request):
 	return render(request,'pest_classification/loggedin.html', {"username":username})
 
 	##-------------- file upload ---------
+## change it not good practice
+uploaded_file_url=""
 
 def simple_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
+        global uploaded_file_url
         uploaded_file_url = fs.url(filename)
         #return render(request, 'pest_classification/simple_upload.html', {
          #   'uploaded_file_url': uploaded_file_url
         #})
-        return render(request, 'pest_classification/pest_detection_view.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
+        #return render(request, 'pest_classification/pest_detection_view.html', {
+        #    'uploaded_file_url': uploaded_file_url
+        #})
+        return pest_detection(request)
 
     return render(request, 'pest_classification/simple_upload.html')
 
 def pest_detection(request):
-	import test
+	#import test
+
+	import sys
+	sys.path.insert(0,'/home/devil/tensorflow/tf_files')
+	import label_fun
+
+	#print "all this work is waste oooh lalalallaaaaa"
+	y=" script integration successful"
+	labels = "/home/devil/tensorflow/tf_files/retrained_labels.txt"
+	graph = "/home/devil/tensorflow/tf_files/retrained_graph.pb"
+	image = "/home/devil/tensorflow/tf_files/testing_images/betal/test1.jpg"
 	#if request.method == 'POST':
-	
-	output = test.hello()
+	#return y + " " + label_fun.label(labels, image, graph)
+	output = y + " " + label_fun.label(labels, image, graph)
+	#output = test.hello() + ' -> ' + uploaded_file_url
 	return HttpResponse(output,content_type = "text/plain")
 	#return render(request, 'pest_classification/pest_query_response.html', {'output': output})
 
