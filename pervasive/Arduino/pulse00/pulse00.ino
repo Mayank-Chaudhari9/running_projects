d0188ac7a0a9361eca916e08a567d4fe0139d829
Mayank-Chaudhari9 <m.charly3015@gmail.com>
2018-01-11 23:32:56 +0530

    pervasive test

---
+++ pervasive/Arduino/pulse00/pulse00.ino
@@ -0,0 +1,36 @@
+int analogPin = 0; // taking input from pulse sensor;
+
+int val = 0; // storing intitial read;
+int s_min = 1008; // sensor min val
+int s_max = 0; // sensor max_val
+
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  // caliberating the output for first 10sec
+
+  while(millis() <100)
+  {
+     val= analogRead(analogPin);
+
+    // value caliberation  code for sensor values;
+     if(s_max < val)
+        s_max=val;
+     if(val < s_min)
+        s_min =val;
+  }
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  val = analogRead(analogPin);
+  val = map(val,s_min,s_max,0,100);
+  val = constrain(val,0,100);
+  Serial.println(val);
+  delay(500);
+
+}
---
+++ pervasive/Arduino/sketch_oct26a/sketch_oct26a.ino
@@ -0,0 +1,22 @@
+int counter  = 0;
+int pin =13;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin,LOW);
+ }
+
+void loop()
+{
+  counter = counter +1;
+
+  digitalWrite(pin,HIGH);
+  Serial.print("Blink #");
+  Serial.println(counter);
+  delay(1000);
+  digitalWrite(pin,LOW);
+  delay(1000);
+
+}
